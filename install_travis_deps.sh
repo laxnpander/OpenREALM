27d23707cb78be06b76b2e7d251e3333839965d8
Yogesh Khedar <y.khedar@tu-braunschweig.de>
2020-05-27 22:22:14 +0200

    Limited the make cpu to 1 so that travis does not complain about memory issues.

--- install_travis_deps.sh
+++ install_travis_deps.sh
@@ -2,7 +2,7 @@
 set -ex
 
 cd ~ && mkdir Pangolin && cd Pangolin && git clone https://github.com/stevenlovegrove/Pangolin.git && \
-            cd Pangolin && mkdir build && cd build && cmake .. && make -j && make install
+            cd Pangolin && mkdir build && cd build && cmake .. && make -j 1 && make install
 
 mkdir -p ~/cmake_ws/src && cd ~/cmake_ws/src && git clone -b cmake_only https://github.com/laxnpander/OpenREALM.git && \
-cd OpenREALM && mkdir build && cd build && cmake .. && make
+cd OpenREALM && mkdir build && cd build && cmake .. && make -j 1
