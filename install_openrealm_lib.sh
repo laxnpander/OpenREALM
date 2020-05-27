abfb3473efe54deda7f88d997496959617cdc998
Yogesh Khedar <y.khedar@tu-braunschweig.de>
2020-05-27 22:41:55 +0200

    install scripts modified to use only one script to install the deps normally and in the travis as well.

--- install_openrealm_lib.sh
+++ install_openrealm_lib.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -ex
 
-cd ~ && mkdir Pangolin && cd Pangolin && git clone https://github.com/stevenlovegrove/Pangolin.git && \
-            cd Pangolin && mkdir build && cd build && cmake .. && make -j 1 && make install
-
 mkdir -p ~/cmake_ws/src && cd ~/cmake_ws/src && git clone -b cmake_only https://github.com/laxnpander/OpenREALM.git && \
 cd OpenREALM && mkdir build && cd build && cmake .. && make -j 1
