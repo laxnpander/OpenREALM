6348e1dd039a1839636e01985c29e3fde72631ff
alex <laxnpander@gmail.com>
2020-09-24 09:25:41 +0200

    Attempt to reduce OpenCV Travis build time

--- tools/install_opencv.sh
+++ tools/install_opencv.sh
@@ -3,5 +3,5 @@ set -ex
 
 cd ~ && git clone -b 3.3.1 https://github.com/opencv/opencv.git \
 && cd ~/opencv && mkdir build && cd build \
-&& cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local .. \
+&& cmake -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D CMAKE_INSTALL_PREFIX=/usr/local .. \
 && make && sudo make install
