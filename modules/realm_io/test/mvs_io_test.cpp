6be8d2d6a6571a8a79f06dbd84b4ab03991d7950
alex <laxnpander@gmail.com>
2021-05-07 11:44:17 +0200

    Removed couts in test and added todo

--- modules/realm_io/test/mvs_io_test.cpp
+++ modules/realm_io/test/mvs_io_test.cpp
@@ -9,18 +9,8 @@
 
 using namespace realm;
 
+// TODO
 TEST(MvsIO, LoadMvs)
 {
-  io::MvsInterface interface;
-  io::MvsArchive::SerializeLoad(interface, "/home/alex/Documents/output/21-03-19_17-40-50/mosaicing/mvs/data.mvs");
 
-  io::MvsInterface::Platform platform = interface.platforms.front();
-  std::cout << platform.cameras.front().name << std::endl;
-  std::cout << platform.cameras.front().K << std::endl;
-  std::cout << platform.poses.size() << std::endl;
-  std::cout << platform.poses.front().R << std::endl;
-  for (const auto& i : interface.images)
-  {
-    std::cout << i.name << std::endl;
-  }
 }
